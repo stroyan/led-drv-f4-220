f5f9479c06f34fcfca9112314cab4ebf71cf7b50
Mike Stroyan <mike@stroyan.net>
2019-02-07 16:58:09 -0700

    A ioremap and write function mapped IO.
    This makes lights work for root writing commands into /dev/leds.

--- leds-f4-220.c
+++ leds-f4-220.c
@@ -4,12 +4,12 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/types.h>
 #include <linux/device.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>	/* for get_user and put_user */
-#include <linux/printk.h>
+
+static void volatile __iomem *gpbcon[12];
 
 #define DEBUG 1
 
@@ -22,6 +22,34 @@ static int leds_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
+static void turn_on(void volatile __iomem *addr)
+{
+		u64 reg;
+		reg = readq(addr);
+		reg &= 0x11111101;
+		writeq(reg, addr);
+		reg = readq(addr);
+		reg |= 0x1;
+		writeq(reg, addr);
+		reg = readq(addr);
+		reg |= 0x10;
+		writeq(reg, addr);
+}
+
+static void turn_off(void volatile __iomem *addr)
+{
+		u64 reg;
+		reg = readq(addr);
+		reg &= 0x11111101;
+		writeq(reg, addr);
+		reg = readq(addr);
+		reg &= ~0x1ul;
+		writeq(reg, addr);
+		reg = readq(addr);
+		reg |= 0x10;
+		writeq(reg, addr);
+}
+
 static ssize_t leds_write(struct file *file, const char __user *buf,
 				size_t count, loff_t *pos)
 {
@@ -32,6 +60,78 @@ static ssize_t leds_write(struct file *file, const char __user *buf,
 		return -EFAULT;
 	buffer[count] = 0;
 	printk(KERN_INFO "leds_write(%s)\n", buffer);
+	if (!strcmp("led1on", buffer)) {
+		turn_on(gpbcon[0]);
+	}
+	else if (!strcmp("led1off", buffer)) {
+		turn_off(gpbcon[0]);
+	}
+	else if (!strcmp("led2on", buffer)) {
+		turn_on(gpbcon[1]);
+	}
+	else if (!strcmp("led2off", buffer)) {
+		turn_off(gpbcon[1]);
+	}
+	else if (!strcmp("led3on", buffer)) {
+		turn_on(gpbcon[2]);
+	}
+	else if (!strcmp("led3off", buffer)) {
+		turn_off(gpbcon[2]);
+	}
+	else if (!strcmp("led4on", buffer)) {
+		turn_on(gpbcon[3]);
+	}
+	else if (!strcmp("led4off", buffer)) {
+		turn_off(gpbcon[3]);
+	}
+	else if (!strcmp("led5on", buffer)) {
+		turn_on(gpbcon[4]);
+	}
+	else if (!strcmp("led5off", buffer)) {
+		turn_off(gpbcon[4]);
+	}
+	else if (!strcmp("led6on", buffer)) {
+		turn_on(gpbcon[5]);
+	}
+	else if (!strcmp("led6off", buffer)) {
+		turn_off(gpbcon[5]);
+	}
+	else if (!strcmp("led7on", buffer)) {
+		turn_on(gpbcon[6]);
+	}
+	else if (!strcmp("led7off", buffer)) {
+		turn_off(gpbcon[6]);
+	}
+	else if (!strcmp("led8on", buffer)) {
+		turn_on(gpbcon[7]);
+	}
+	else if (!strcmp("led8off", buffer)) {
+		turn_off(gpbcon[7]);
+	}
+	else if (!strcmp("led9on", buffer)) {
+		turn_on(gpbcon[8]);
+	}
+	else if (!strcmp("led9off", buffer)) {
+		turn_off(gpbcon[8]);
+	}
+	else if (!strcmp("led10on", buffer)) {
+		turn_on(gpbcon[9]);
+	}
+	else if (!strcmp("led10off", buffer)) {
+		turn_off(gpbcon[9]);
+	}
+	else if (!strcmp("led11on", buffer)) {
+		turn_on(gpbcon[10]);
+	}
+	else if (!strcmp("led11off", buffer)) {
+		turn_off(gpbcon[10]);
+	}
+	else if (!strcmp("led12on", buffer)) {
+		turn_on(gpbcon[11]);
+	}
+	else if (!strcmp("led12off", buffer)) {
+		turn_off(gpbcon[11]);
+	}
 	return count;
 }
 
@@ -66,17 +166,31 @@ int init_module(void){
 	printk(KERN_INFO "TerraMaster F4-220 LED module loaded.\n");
 #endif
 
-	/*
-	 * A non 0 return means init_module failed; module can't be loaded.
-	 */
+	gpbcon[0] = ioremap_nocache(0xfed0d1b8, 16);
+	printk(KERN_INFO "ioremap_nocache(0xfed0d1b8,16) returned %p\n",gpbcon[0]);
+	gpbcon[1] = ioremap_nocache(0xfed0d028, 16);
+	gpbcon[2] = ioremap_nocache(0xfed0d0b8, 16);
+	gpbcon[3] = ioremap_nocache(0xfed0d0d8, 16);
+	gpbcon[4] = ioremap_nocache(0xfed0d088, 16);
+	gpbcon[5] = ioremap_nocache(0xfed0d0a8, 16);
+	gpbcon[6] = ioremap_nocache(0xfed0d048, 16);
+	gpbcon[7] = ioremap_nocache(0xfed0d068, 16);
+	gpbcon[8] = ioremap_nocache(0xfed0d018, 16);
+	gpbcon[9] = ioremap_nocache(0xfed0d038, 16);
+	gpbcon[10] = ioremap_nocache(0xfed0d058, 16);
+	gpbcon[11] = ioremap_nocache(0xfed0d098, 16);
+
 	return 0;
 }
 
 void cleanup_module(void)
 {
+	int i;
 #ifdef DEBUG
 	printk(KERN_INFO "TerraMaster F4-220 LED module unloaded.\n");
 #endif
+	for (i = 0; i<12; i++)
+		iounmap(gpbcon[i]);
 	misc_deregister(&leds_miscdev);
 }
 
